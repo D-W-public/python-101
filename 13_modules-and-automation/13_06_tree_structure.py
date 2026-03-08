fd876c238226040454bfea907884ce5e7ba1c04c
Ishvar <ishvar108@proton.me>
2026-03-08 20:13:54 +0530

    m

--- 13_modules-and-automation/13_06_tree_structure.py
+++ 13_modules-and-automation/13_06_tree_structure.py
@@ -1,3 +1,10 @@
-# Write a script that walks through a nested folder structure 
+# Write a script that walks through a nested folder structure
 # and prints out all the Python files it can find.
 # Run it in your labs folder and add formatting for nicer viewing.
+
+from pathlib import Path
+
+labs = Path.home() / "CodingNomads" / "python-101"
+
+for file in labs.rglob("*.py"):
+    print(file.name)
